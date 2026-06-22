766cfe1387bce924f759f435bf2bf8d703dafe4d
Joomo1210 <ojo.jojo88@gmail.com>
2026-06-22 20:03:22 +0100

    Integrate BlogIndex in Router for blog routes
    Add BlogIndex component to Router for blog path handling

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import BlogIndex from './BlogIndex.jsx'
 import BlogPost from './BlogPost.jsx'
 import AdminNewPost from './AdminNewPost.jsx'
 
 function Router() {
   const path = window.location.pathname;
 
+  if (path === '/blog' || path === '/blog/') {
+    return <BlogIndex />;
+  }
   if (path.startsWith('/blog/')) {
     return <BlogPost />;
   }
